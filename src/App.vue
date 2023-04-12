92d86a4c597958d8b85cf86292247eb3068a93cf
peppeluc22 <gluca0260@gmail.com>
2023-04-12 13:47:11 +0200

    added hero component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <header>
-    <AppTitle />
-  </header>
+  <body>
+    <header>
+      <AppTitle />
+    </header>
+
+    <main>
+      <AppHero />
+    </main>
+  </body>
 </template>
 
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 export default {
   components: {
-    AppTitle,
+    AppTitle, AppHero
   }
 }
 </script>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="hero">
+        <img src="../assets/messina..png" alt="messina">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHero'
+}
+</script>
